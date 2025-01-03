8eeed670ff7a569b69624cd842c1c1c56ab11d95
Mikhail <mihail.senotrusov@gmail.com>
2025-01-03 17:55:38 +0800

    2 commit text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,40 +1,19 @@
-function saveItemLocalStorage(key, value) {
-    if (typeof value !== 'string') {
-      localStorage.setItem(key, JSON.stringify(value));
-    } else {
-      localStorage.setItem(key, value);
-    }
-  }
-  
-  function actionsWithEditor() {
-    const editor = document.getElementById('editor');
-    const clearEditorBtn = document.getElementById('editor__clear-btn');
-    const keyOfEditor = 'textInEditor';
-  
-    if (localStorage.getItem(keyOfEditor)) {
-      editor.value = localStorage.getItem(keyOfEditor);
-    }
-  
-    editor.addEventListener('input', () => {
-      // Если пользователь очистил поле (не используя кнопку "Очистить содержимое"):
-      if (!editor.value) {
-        localStorage.removeItem(keyOfEditor);
-        return;
-      } 
-  
-      // Если поле заполнено только пробелами:
-      if (editor.value.match(/^\s*$/)) return;
-  
-      saveItemLocalStorage(keyOfEditor, editor.value.trim());
-    });
-  
-    clearEditorBtn.addEventListener('click', () => {
-      if (!editor.value.match(/^\s*$/)) {
-        localStorage.removeItem(keyOfEditor);
-      }
-  
-      editor.value = '';
-    });
-  }
-  
-  actionsWithEditor();
+const storedText = localStorage.getItem('text');
+const textEditor = document.getElementById('editor');
+
+// Загрузка текста из localStorage
+if(storedText !== null) {
+    textEditor.value = storedText;
+}
+
+// Редактирование текста 
+textEditor.addEventListener('input', () => {
+    localStorage.setItem('text', textEditor.value);
+})
+
+// Кнопка удаления текста
+const removeBtn = document.getElementById('remove_btn');
+removeBtn.addEventListener('click', () => {
+    textEditor.value = '';
+    localStorage.removeItem('text');
+})
