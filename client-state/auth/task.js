b91f1b1034ab7291810c6923942cc2643d4eb230
Mikhail <mihail.senotrusov@gmail.com>
2025-01-05 16:09:54 +0800

    3 commit auth

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,48 +1,68 @@
 const form = document.getElementById('signin__form');
-const welcome = document.getElementById('welcome');
+const xhr = new XMLHttpRequest();
+let userId = document.getElementById('user_id');
 const signin = document.getElementById('signin');
-const logoutBtn = document.getElementById('logout__btn');
+const welcome = document.getElementById('welcome');
 
-// Событие изменения в форме авторизации и отправка запроса на сервер
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
 
-    const formData = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-        if(xhr.response.success) {
-            const userId = xhr.response.user_id;
-            localStorage.setItem('userId', userId);
-            logoutBtn.classList.add('logout__btn_active');
-            welcomeDude(userId);
-        } else {
-            alert('Неверный логин или пароль');
-        }
-    }        
-    xhr.send(formData);
-    form.reset();
+document.addEventListener('DOMContentLoaded', function() {
+    
+    if (localStorage.getItem('user_id')) {
+        showWelcome();
+
+    } else {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+        
+            const formData = new FormData(form);
+        
+            xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+            xhr.responseType = 'json';
+            xhr.send(formData);
+        
+            xhr.onload = function() {
+
+                if(!xhr.response.success) {
+                    alert('Неверный логин/пароль');
+
+                } else {
+                    //console.log(xhr.response);
+                    
+                    localStorage.setItem('user_id', xhr.response['user_id']);
+                    showWelcome();
+                };
+            };
+
+            form.reset();
+        });
+    };
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-    const userId = localStorage.getItem('userId');
-    if(userId) {
-        welcomeDude(userId);
-    }
-});
 
-// Событие клика на кнопку Выйти
-logoutBtn.addEventListener('click', () => {
-    localStorage.removeItem('userId');
-    signin.classList.add('signin_active');
-    welcome.classList.remove('welcome_active');
-    logoutBtn.classList.remove('logout__btn_active')    
-})
 
-// Функция приветствия пользователя 
-function welcomeDude(userId) {
-    signin.classList.remove('signin_active');
-    welcome.classList.add('welcome_active');
-    welcome.querySelector('#user_id').textContent = userId;
-}
+function showWelcome() {
+     
+     welcome.classList.add('welcome_active');
+     signin.classList.remove('signin_active');
+
+     userId.textContent = localStorage.getItem('user_id');
+
+     welcome.insertAdjacentHTML('afterend', `
+        <button class="signup_btn" id="signup">Выйти</button>
+        `);
+
+    const signUpBtn = document.getElementById('signup');
+
+    if(signUpBtn) {
+        signUpBtn.addEventListener('click', () => {
+            console.log('signup');
+    
+            welcome.classList.remove('welcome_active');
+            signin.classList.add('signin_active');
+    
+            localStorage.clear();
+            //signUpBtn.remove();
+            location.reload();
+        });
+    };
+};
