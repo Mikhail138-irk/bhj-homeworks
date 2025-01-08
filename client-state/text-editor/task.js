3be2284e58849a04d38ea97dc25da0cfe22dfaef
Mikhail <mihail.senotrusov@gmail.com>
2025-01-08 16:53:01 +0800

    new commit text-editor

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,25 +1,9 @@
-"use strict";
+const editor = document.getElementById('editor');
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-	const editor = document.getElementById("editor");
-	const resetButton = document.getElementById("resetButton");
+editor.value = localStorage.getItem('editor');
 
-    // есть ли сохраненный текст в localStorage
-    const savedText = localStorage.getItem("editorText");
+function StoreEditor(e) {
+  localStorage.setItem('editor', editor.value);
+}
 
-    // обр.соб. изменения текста в редакторе
-    editor.addEventListener("input", () => {
-        // получаем текущий текст из редактора
-        const currentText = editor.value;
-        // сохраняем текст в localStorage
-        localStorage.setItem("editorText", currentText);
-	});
-	// "Сбросить"
-    resetButton.addEventListener("click", () => {
-        // очищаем текстовый редактор
-        editor.value = "";
-        // удаляем сохраненный текст из localStorage
-        localStorage.removeItem("editorText");
-    });
-});
+editor.addEventListener('input', StoreEditor);
